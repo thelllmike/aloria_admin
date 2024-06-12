8bf2fb195ab196019dc6647dd3bb0afe14383919
thelllmike <sachinharshitha179@gmail.com>
2024-06-12 17:26:09 +0530

    fix product detele

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,44 @@
 import React from "react";
 import "../style/Card.css";
 import { NavLink } from "react-router-dom";
+import Swal from "sweetalert2";
+import api from "../apiService";
 
-const Card = ({ product, index }) => {
+const Card = ({ product, index, onProductDeleted }) => {
+    const handleDelete = async () => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Do you want to delete this item?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    const response = await api.delete(`/products/products/${product.product_id}`);
+                    if (response.status === 200) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your item has been deleted.',
+                            'success'
+                        ).then(() => {
+                            // Refresh the page
+                            window.location.reload();
+                        });
+                    }
+                } catch (error) {
+                    Swal.fire(
+                        'Error!',
+                        'There was a problem deleting the item.',
+                        'error'
+                    );
+                }
+            }
+        });
+    };
+
     return (
         <div className="card mt-4">
             <img src={product.images.length > 0 ? product.images[0].image_url : "default_image_url"} alt="Product" className="card-img-top" />
@@ -22,9 +58,9 @@ const Card = ({ product, index }) => {
                     <NavLink to={`/editproduct/${product.product_id}`} className="btn edit-btn">
                         <i className="fas fa-edit"></i> Edit
                     </NavLink>
-                    <NavLink to="#" className="btn delete-btn">
+                    <button onClick={handleDelete} className="btn delete-btn">
                         <i className="fas fa-trash"></i> Delete
-                    </NavLink>
+                    </button>
                 </div>
             </div>
         </div>
